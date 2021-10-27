d8586bb43796954bfb6dd1c53d88274fd946599c
samuelbrinke <samuel.brinkeback@hotmail.com>
2021-10-27 17:31:16 +0200

    Added functions file and css function

--- myTheme/header.php
+++ myTheme/header.php
@@ -5,7 +5,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title><?php the_title(); ?></title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    <?php wp_head(); ?>
-  <link rel="stylesheet" href="wp-content/themes/myTheme/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <?php wp_head(); ?>
 </head>
 <body>
+
+<header>
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid justify-content-center">
+      <img style="width: 50px;" src="https://cryptologos.cc/logos/ethereum-eth-logo.png" alt="" srcset="https://cryptologos.cc/logos/ethereum-eth-logo.png">
+      <a class="navbar-brand" href="#">Navbar</a>
+    </div>  
+  </nav>  
+</header>
