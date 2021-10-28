c5cddde2cbb50b9512d9b4abb2460d3c73410f2f
samuelbrinke <samuel.brinkeback@hotmail.com>
2021-10-28 21:30:07 +0200

    Added navbar with dynamic content

--- myTheme/functions.php
+++ myTheme/functions.php
@@ -4,3 +4,20 @@ function addStyleSheets():void {
 }
 
 add_action('wp_enqueue_scripts', 'addStyleSheets');
+
+function my_function_admin_bar(){ return false; }
+add_filter( 'show_admin_bar' , 'my_function_admin_bar');
+
+function myTheme_custom_logo() {
+    $defaults = array(
+        'height'               => 50,
+        'width'                => 50,
+        'flex-height'          => true,
+        'flex-width'           => true,
+        'header-text'          => array( 'site-title', 'site-description' ),
+        'unlink-homepage-logo' => true, 
+    );
+ 
+    add_theme_support( 'custom-logo', $defaults );
+}
+add_action('after_setup_theme', 'myTheme_custom_logo');
